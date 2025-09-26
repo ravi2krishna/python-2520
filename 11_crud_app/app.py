154227f84dd7afba28f36b15c3f03a4dbd7138c7
ravi2krishna <ravi2krishna@gmail.com>
2025-09-26 08:51:54 +0530

    26th Sep - Student Management System - 2

--- 11_crud_app/app.py
+++ 11_crud_app/app.py
@@ -45,16 +45,85 @@ while True:
                 else:
                     print("Score Should Numbers Only")
             
-        
-        
+            # set for skills
+            skills = set()
+            while True:
+                skill_input = input("Enter Skill or type done: ")
+                if skill_input == "done":
+                    break
+                skills.add(skill_input.title())
+            
+            # save the student details
+            students[student_id] = {
+                "name": name,
+                "scores": scores,
+                "skills": skills
+            }
+            print("Student Saved")
+            
+            # for our confirmation
+            print(students)
+                    
     elif choice == "2":
         print("Updating Student Logic")
+        student_id = input("Enter ID To Update: ")
+        if student_id in students:
+            new_name = input("Enter New Name: ").title()
+            students[student_id]["name"] = new_name
+            print("Student Name Updated")
+        else:
+            print("ID Doesn't Exist to Update")
+        
+        # for our confirmation
+        print(students)
+        
+        
     elif choice == "3":
         print("Deleting Student Logic")
+        student_id = input("Enter ID To Delete: ")
+        if student_id in students:
+            remove = students.pop(student_id)
+            print(remove)
+        else:
+            print("ID Doesn't Exist to Delete")
+        
+        # for our confirmation
+        print(students)
+        
     elif choice == "4":
         print("Listing Students Logic")
+        for sid, data in students.items():
+            name = data["name"]
+            scores = data["scores"]
+            
+            avg = sum(scores)/len(scores)
+            high_score = max(scores)
+            low_score = min(scores)
+            
+            skills = data["skills"]
+            skills_count = len(skills)
+            
+            print("="*50)
+            print("STUDENT DETAILS")
+            print("="*50)
+            
+            print(f"ID: {sid}")
+            print(f"NAME: {name}")
+            print(f"ALL SCORES: {scores}")
+            print(f"AVG SCORE: {avg}")
+            print(f"HIGH SCORE: {high_score}")
+            print(f"LOWEST SCORE: {low_score}")
+            print(f"ALL SKILLS: {skills}")
+            print(f"NO OF SKILLS: {skills_count}")
+            
+        
     elif choice == "5":
         print("Exit System")
+        print("="*50)
+        print("CONTACT ADMIN FOR MORE INFORMATION")
+        print(f"ADMIN CONTACT NO: {ADMIN_INFO[0]}")
+        print(f"ADMIN EMAIL ID: {ADMIN_INFO[1]}")
+        print("="*50)
         break
     else:
         print("Invalid Option, Only Select (1-5)")    

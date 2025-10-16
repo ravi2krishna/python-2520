f7965644517e63434b2a47c165343174e30c642b
ravi2krishna <ravi2krishna@gmail.com>
2025-10-16 08:57:28 +0530

    16th Oct - Instance - Class - Static Members

--- 17_oops/demo.py
+++ 17_oops/demo.py
@@ -55,22 +55,59 @@ student_three_obj.info()
 
 # __init__() -> Constructor
 class Student:
+    
+    # class variable is shared across all objects 
+    institute_name = "Edify"
+    
     # Constructor
     def __init__(self,student_name,student_email):
-        self.student_name = student_name
+        if not Student.validate_email(student_email):
+            raise ValueError("Invalid Email Address")
+        # instance variables
+        self.student_name = student_name 
         self.student_email = student_email    
         
-    # method
+    # instance method
     def info(self):
         # student_email = "john@gmail.com"
+        # Accessing instance variables 
         print("Student Name: ",self.student_name)
         print("Student Email: ",self.student_email) 
         
+        # Accessing class variables 
+        print("Student Institute: ",Student.institute_name) # recommended 
+        # print("Student Institute Via Object: ",self.institute_name) # not recommended 
+    
+    # class method    
+    @classmethod
+    def change_institute(cls,new_name):
+        cls.institute_name = new_name # recommended
+        # Student.institute_name = new_name 
+        # Accessing instance data inside a class method is not possible
+        # print("Accessing Instance Data: ",self.student_name)
+        
+    
+    # static method -> utility 
+    @staticmethod
+    def validate_email(email):
+        return "@" in email and "." in email
+            
 ravi_obj = Student("ravi","ravi@gmail.com")
 john_obj = Student("john","john@gmail.com")
 mike_obj = Student("mike","mike@gmail.com")
-
+print("="*50)
 ravi_obj.info()
 john_obj.info()
 mike_obj.info()
-student_one_obj.info()
+
+# change the institute 
+Student.change_institute("Digital Edify")
+print("="*50)
+ravi_obj.info()
+john_obj.info()
+mike_obj.info()
+
+# calling static method 
+print(Student.validate_email("test@gmail.com"))
+print(Student.validate_email("test"))
+

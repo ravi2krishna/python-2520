f6ececbaa1cd50ef13cfc9a3872ddaba405034f2
ravi2krishna <ravi2krishna@gmail.com>
2025-10-17 09:05:49 +0530

    17th Oct - LMS App with OOPS - 1

---
+++ 18_oops_style/student.py
@@ -0,0 +1,79 @@
+# Student Class 
+
+class Student:
+    
+    # class variable -> class data common to all students
+    institute_name = "Edify"
+    
+    # instance data specific to each student 
+    def __init__(self,student_id,student_name,student_age,student_email,student_mobile_number):
+        # identity info
+        self.student_id = student_id
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_email = student_email
+        self.student_mobile_number = student_mobile_number
+        
+        # calculations info
+        self.total_sessions_attended = 0
+        self.attendance_credits = 0
+        self.performance_credits = 0
+        self.final_credits = 0
+        self.trainer_rating = 0
+
+    # hover like functionality -> display basic info 
+    def student_basic_info(self):
+        print("========== Basic Student Info ==========")
+        print(f"Student ID : {self.student_id}")
+        print(f"Student Name : {self.student_name}")
+    
+    # click like functionality -> display complete info 
+    def student_complete_info(self):
+        print("========== Basic Complete Info ==========")
+        print(f"Student ID : {self.student_id}")
+        print(f"Student Name : {self.student_name}")
+        print(f"Student Age : {self.student_age}")
+        print(f"Student Email : {self.student_email}")
+        print(f"Student Mobile No : {self.student_mobile_number}")
+        
+    # calculate sessions attended credits 
+    def sessions_credits_cal(self):
+        total_sessions_attended = int(input("Enter Total Sessions Attended: "))
+        self.total_sessions_attended = total_sessions_attended
+        
+        if total_sessions_attended >= 30:
+            self.attendance_credits += 5
+        elif total_sessions_attended >= 20:
+            self.attendance_credits += 3
+        else:
+            self.attendance_credits = 0
+        return self.attendance_credits
+    
+
+    # calculate performance credits based o score 
+    def performance_credits_cal(self, score):
+        if score >= 85:
+            self.performance_credits += 5
+        elif score >= 60:
+            self.performance_credits += 3
+        else:
+            self.performance_credits = 0
+        return self.performance_credits
+    
+    # calculate achievement (based on above two calculations)
+    def achievement_status(self):
+        self.final_credits = self.sessions_credits_cal() + self.performance_credits_cal(90)
+        if self.final_credits >= 10:
+            print("Got 🥇")
+        elif self.final_credits >= 8:
+            print("Got 🥈")
+        else:
+            print("Got 🥉")
+            
+    # give trainer rating 
+    def trainer_rating_cal(self):
+        self.trainer_rating = int(input("Enter Trainer Rating (1-5): "))
+        if self.trainer_rating >=5 :
+            return 5000
+        else:
+            return 0
---
+++ 18_oops_style/trainer.py
@@ -0,0 +1,38 @@
+# Trainer Class 
+
+from student import Student
+
+class Trainer:
+    
+    # instance data specific to each trainer 
+    def __init__(self,trainer_id,trainer_name):
+        # identity info
+        self.trainer_id = trainer_id
+        self.trainer_name = trainer_name
+        
+        # calculations info
+        self.total_sessions_taken = 0
+        self.payment_for_sessions = 0
+        self.training_bonus = 0
+        self.total_payment = 0
+        
+    # trainer info
+    def trainer_info(self):
+        print("========== Trainer Info ==========")
+        print(f"Trainer ID : {self.trainer_id}")
+        print(f"Trainer Name : {self.trainer_name}")
+        
+    # trainer payment calculation
+    def sessions_payment_cal(self):
+        self.total_sessions_taken = int(input("Enter Total Sessions Taken: "))
+        base_pay_per_session = 2000
+        self.payment_for_sessions = self.total_sessions_taken * base_pay_per_session
+        
+        # get student rating here 
+        s1 = Student()
+        print("========= Student Rating Given =========")
+        self.training_bonus = s1.trainer_rating_cal()
+        self.total_payment = self.payment_for_sessions + self.training_bonus
+        print("========= Trainer Payment =========")
+        print(f"Total Trainer Payment: {self.total_payment}")
+        
